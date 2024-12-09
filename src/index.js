7f2bcf772db031509d8e463ee9ad483e0c8f2d16
tanmay0996 <167513949+tanmay0996@users.noreply.github.com>
2024-12-09 19:12:52 +0530

    dotenc.config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // require('dotenv').config({path:'./env'})
-// import dotenv from "dotenv"
+import dotenv from "dotenv"
 
 import connectDB from "./db/index.js"
 
-// dotenv.config({
-//     path:'./env'
-// })
+dotenv.config({
+    path:'./env'
+})
 
 
 connectDB()

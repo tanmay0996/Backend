7898275708102bd66fe60677bcbd27e21e4cf6cd
tanmay0996 <167513949+tanmay0996@users.noreply.github.com>
2025-02-11 21:59:04 +0530

    like.routes

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import userRouter from "./routes/user.routes.js"  //coustomize name
 import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/likes.routes.js";
 
 
 
@@ -36,6 +37,8 @@ app.use("/api/v1/tweet", tweetRouter);
 
 app.use("/api/v1/video", videoRouter);
 
-app.use("/api/v1/comment", commentRouter)
+app.use("/api/v1/comment", commentRouter);
+
+app.use("/api/v1/likes", likeRouter);
 
 export {app}
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import {Like} from "../models/like.model.js"
 // import {Comment} from "../models/comment.model.js"
 import { isValidObjectId } from "mongoose";
-import ApiError from "../utils/ApiError.js"
-import ApiResponse from "../utils/ApiResponse.js"
-import asyncHandler from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike=asyncHandler(async (req,res) => {
     const {videoId}=req.params
